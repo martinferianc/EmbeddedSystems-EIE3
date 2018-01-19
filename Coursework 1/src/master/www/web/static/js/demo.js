9090bba2e9c75b1fcbe0cc046b1e5b9976aee685
Martin Ferianc <ferianc.martin@gmail.com>
2018-01-19 22:37:27 +0000

    [cw1] on click works for all players

--- Coursework 1/src/master/www/web/static/js/demo.js
+++ Coursework 1/src/master/www/web/static/js/demo.js
@@ -1,5 +1,8 @@
 $(document).ready ( function () {
-    $(document).on ("click", ".player", function () {
-        alert("hi");
+    $(document).on ("click", "#1,#2,#3,#4,#5,#6,#7,#8,#9,#10,#11,#12,#13,#14,#15,#16,#17,#18,#19", function () {
+        var src = ($(this).attr('src') === "/static/img/no.png" )
+           ? "/static/img/yes.png"
+           : "/static/img/no.png";
+        $(this).attr('src', src);
     });
 });
