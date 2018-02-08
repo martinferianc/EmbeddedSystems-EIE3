ea88c69a7fbadca8c657913fd2e5859ec9069c5a
Alexander Luisi <al3515@ic.ac.uk>
2018-02-08 09:27:25 +0000

    Two terminals

--- Coursework 1/src/master/main.py
+++ Coursework 1/src/master/main.py
@@ -13,23 +13,8 @@ if __name__ == '__main__':
     HOST = sys.argv[1]
     PORT = int(sys.argv[2])
 
-
     #### ALL ESTABLISHING CODE BEFORE THESE LINES ###
     app = create_app('dev')
-
-    def on_connect(client, userdata, flags, rc):
-        print("Connected with result code "+str(rc))
-        client.subscribe("esys/headaid/#")
-
-    def on_message(client, userdata, msg):
-         print(str(msg.payload))
-
-    #Establish connection
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect("192.168.0.10", 1883, 60)
-    client.loop_start()
 
     if DEBUG:
         app.run(debug=True, host=HOST,port=PORT, threaded=True)
@@ -38,10 +23,8 @@ if __name__ == '__main__':
         app.run(debug=True, host=HOST,port=PORT, threaded=True)
     _thread.start_new_thread(web_thread,())
 
-
     # Main Loop
     while True:
         #Write to the team.json any changes
         # Deserialize the json here
-
         pass
