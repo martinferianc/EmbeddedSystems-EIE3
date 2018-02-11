9bf58f16999b5b0b1ce0ca3db1c7d21624ee25e3
Alex Montgomerie <AlexMontgomerie@github.com>
2018-02-11 23:57:30 +0000

    added data logging capability

--- Coursework 1/src/master/main.py
+++ Coursework 1/src/master/main.py
@@ -3,11 +3,27 @@ import random
 import time
 from www.web import create_app
 import sys
-import paho.mqtt.client as mqtt
-import _thread
-
+#import paho.mqtt.client as mqtt
+#import _thread
+import json
+import csv
 
 DEBUG = True
+SAVE  = True
+
+def saveData(msg):
+    #load json file
+    data = json.loads(msg.payload)
+    #get timestamp
+    data['TIMESTAMP'] = time.ctime()
+    #leave empty label key
+    data['LABEL'] = 0     
+    tmp = data['DATA']
+    tmp = {'ACX':tmp[0], 'ACY':tmp[1], 'ACZ':tmp[2], 'GYX':tmp[4], 'GYY':tmp[5], 'GYZ':tmp[6]}
+    data['DATA'] = tmp
+    with open('../../data/data_raw.txt','a') as outfile:
+        json.dump(data,outfile)
+        outfile.write('\n')
 
 if __name__ == '__main__':
     HOST = sys.argv[1]
@@ -21,9 +37,15 @@ if __name__ == '__main__':
         client.subscribe("esys/headaid/#")
 
     def on_message(client, userdata, msg):
-         print(str(msg.payload))
-         # Deserialize the JSON here
-         # Write to the team.json any changes
+        print(str(msg.payload))
+        # Deserialize the JSON here
+        # Write to the team.json any changes
+        
+        if SAVE:
+            saveData(msg)            
+        
+        return
+      
 
     #Establish connection
     client = mqtt.Client()
@@ -42,6 +64,5 @@ if __name__ == '__main__':
     # Main Loop
     while True:
         #print("potato")
-
-
         pass
+
