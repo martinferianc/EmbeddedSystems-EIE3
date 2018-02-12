3996fa3cbec86af8a50fafb22222e0ef47800781
Alex Montgomerie <AlexMontgomerie@github.com>
2018-02-12 11:45:16 +0000

    added training script

--- Coursework 1/src/master/train.py
+++ Coursework 1/src/master/train.py
@@ -1,20 +1,34 @@
 import json
-import csv
 from algorithms.kmeans import KMeans
 
 def preprocessData(testRatio):
-    #load csv
     #features to X
-    #label to Y (last element in line)
+    X = []
+    Y = []
 
+    with open('../../data_raw.txt','r') as f:
+        for line in f:
+            data = json.loads(line)
+            X.append([data['ACX'],data['ACY'],data['ACZ'],data['GYX'],data['GYY'],data['GYZ']])
+            Y.append(data['LABEL'])
+
+    testSize = int(testRatio*len(Y))    
+
+    train_X = X[0:testSize-1]
+    train_Y = Y[0:testSize-1]
+    
+    test_X = X[testSize:]
+    test_Y = Y[testSize:]
+
+    return train_X, train_Y, test_X, test_Y
+
+#routine for user to label data
 def labelData():
     pass
 
-def toCSV(data):
-    pass
-
 def train(): 
- #train_X, train_Y, test_X, test_Y = preprocess_data("../../data/titanic/train.csv",0.1)
-#c = KMeans( k=2, tol=0.00001, max_iter=3000)
-#c.fit(train_X,train_Y)
-#c.test(test_X, test_Y)  
+    #labelData()
+    train_X, train_Y, test_X, test_Y = preprocessData(0.1)
+    c = KMeans( k=2, tol=0.00001, max_iter=3000)
+    c.fit(train_X,train_Y)
+    c.test(test_X, test_Y)  
