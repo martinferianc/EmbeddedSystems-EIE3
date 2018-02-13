0300bf885ec7204f69c93c17123fbb7c1cac0b08
Owen Harcombe <owen.harcombe@gmail.com>
2018-02-13 15:19:22 +0000

    Data converted

--- Coursework 1/src/master/algorithms/postprocessing.py
+++ Coursework 1/src/master/algorithms/postprocessing.py
@@ -1,28 +1,29 @@
-import math, json
+import math, json, ast, re
 
-class PostProcessing(self):
+class PostProcessing():
 ##################### Member Variables ##########################
 
-    # Gyro calibration values
-    self.gyro_cal_x = 0
-    self.gyro_cal_y = 0
-    self.gyro_cal_z = 0
+    def __init__(self):
+        # Gyro calibration values
+        self.gyro_cal_x = 0
+        self.gyro_cal_y = 0
+        self.gyro_cal_z = 0
 
-    self.yaw = 0;
-    self.roll = 0;
-    self.pitch = 0;
+        self.yaw = 0
+        self.roll = 0
+        self.pitch = 0
 
-    self.average_number = 1000
+        self.average_number = 1000
 
-    # Factor used to decide how much of old angle is used
-    self._average_factor = 0.962
-    # Time step between program running
-    self._time_step = 0.125
-    # Time constant
-    self.tau = (time_step*average_factor)/(1-average_factor)
+        # Factor used to decide how much of old angle is used
+        self._average_factor = 0.962
+        # Time step between program running
+        self._time_step = 0.125
+        # Time constant
+        self.tau = (self._time_step*self._average_factor)/(1-self._average_factor)
 
-    # Convert radians to degrees
-    self._rad2deg = 180/math.pi
+        # Convert radians to degrees
+        self._rad2deg = 180/math.pi
 
 
 ##################### Member Functions ##########################
@@ -30,17 +31,15 @@ class PostProcessing(self):
     # Normalise the acceleration values
     def _acc_normal(self, value, offset = 0):
         # Make negative if req.
-        if(value > 2**15){
+        if(value > 2**15):
             value = value - 2**16
-        }
         # Return normal
         return (value-offset)/2048
 
     def _gyro_normal(self, value, offset = 0):
         # Make neg if req.
-        if(value > 2**15){
+        if(value > 2**15):
             value = value - 2**16
-        }
         # Return normal
         return (value-offset)/16.4
 
@@ -59,36 +58,38 @@ class PostProcessing(self):
         self.yaw = feature_vector['GYZ']
         return {'YAW': yaw, 'PITCH': pitch, 'ROLL': roll}
 
-
-
 ##################### Public Functions ##########################
 
     # Saves the calibration variables into a json file
-    def save_gyro_calibration(self, file_path = '\..\calibration\calibration_values.txt'):
+    def save_gyro_calibration(self, file_path = "calibration/calibration_values.txt"):
         offset_values = {"GYOFX": self.gyro_cal_x, "GYOFY": self.gyro_cal_y, "GYOFZ": self.gyro_cal_z}
-        file = open(file_path, 'w')
-        file.write(offset_values)
-        file.close()
+
+        f = open(file_path, 'w')
+        f.write(str(offset_values))
+        f.close()
 
     # Loads the json file with the calibrated values
-    def load_gyro_calibration(self, file_path = '\..\calibration\calibration_values.txt' ):
-        file = open(file_path, 'r')
-        offset_values = file.readline()
-        self.gyro_cal_x = offset_values['GYX']
-        self.gyro_cal_y = offset_values['GYY']
-        self.gyro_cal_y = offset_values['GYZ']
-        file.close()
+    def load_gyro_calibration(self, file_path = "calibration/calibration_values.txt"):
+        f = open(file_path, 'r')
+        offset_values = ast.literal_eval(f.read())
+        self.gyro_cal_x = offset_values['GYOFX']
+        self.gyro_cal_y = offset_values['GYOFY']
+        self.gyro_cal_y = offset_values['GYOFZ']
+        f.close()
 
     # Calibrate the gyros by taking 2000 samples and then creating an average offset
     def refresh_calibration_values(self, data):
+        tmp_gyro_cal_x = 0
+        tmp_gyro_cal_y = 0
+        tmp_gyro_cal_z = 0
         for i in range(0, self.average_number):
             # Average first 1000 values to find gyro offset
             tmp_gyro_cal_x = tmp_gyro_cal_x + data[i]['GYX']
             tmp_gyro_cal_y = tmp_gyro_cal_y + data[i]['GYY']
             tmp_gyro_cal_z = tmp_gyro_cal_z + data[i]['GYZ']
-        self.gyro_cal_x = tmp_gyro_cal_x/average_number
-        self.gyro_cal_y = tmp_gyro_cal_y/average_number
-        self.gyro_cal_z = tmp_gyro_cal_z/average_number
+        self.gyro_cal_x = tmp_gyro_cal_x/self.average_number
+        self.gyro_cal_y = tmp_gyro_cal_y/self.average_number
+        self.gyro_cal_z = tmp_gyro_cal_z/self.average_number
 
     # Converts the values into actual meaningful units
     def postprocess_data(self, feature_vector, yaw_pitch_roll_values = False):
@@ -103,16 +104,17 @@ class PostProcessing(self):
         return feature_vector
 
     # Process the whole JSON fie
-    def postprocess_file(self, filename, new_gyro_cal = False, old_gyro_cal = False, save_gyro_cal = False):
+    def postprocess_file(self, filename, new_gyro_cal = False, old_gyro_cal = False, save_gyro_cal = False, save_to_file = False):
         raw_data = []
         processed_data = []
+        processed_data_str = ""
         times = []
         # Load in all the JSON from the file
         with open(filename, 'r') as f:
             for line in f:
                 tmp_data = json.loads(line)
                 sensor_data = tmp_data['DATA']
-                data.append(sensor_data)
+                raw_data.append(sensor_data)
                 times.append(tmp_data['TIMESTAMP'])
         if(old_gyro_cal):
             # Iterate through the array and normalise all of the values
@@ -134,7 +136,7 @@ class PostProcessing(self):
                 raw_data[i]['GYZ'] = self._gyro_normal(raw_data[i]['GYZ'])
         if(new_gyro_cal):
             # Take an average of a set number of samples to get the gyro offset
-            calibrate(raw_data)
+            self.refresh_calibration_values(raw_data)
             # Pass back over the array and remove the offset from all the samples
             for i in range(0, len(raw_data)):
                 raw_data[i]['GYX'] = self._gyro_normal(raw_data[i]['GYX'], self.gyro_cal_x)
@@ -142,8 +144,26 @@ class PostProcessing(self):
                 raw_data[i]['GYZ'] = self._gyro_normal(raw_data[i]['GYZ'], self.gyro_cal_z)
         # Put the data in the correct form to be outputted
         for i in range(0, len(raw_data)):
-            processed_data.append([raw_data[i]['ACX'],raw_data[i]['ACY'],raw_data[i]['ACZ'],raw_data[i]['GYX'],raw_data[i]['GYY'],raw_data[i]['GYZ']])
+            feature_vector = {'ACX': raw_data[i]['ACX'], 'ACY':raw_data[i]['ACY'], 'ACZ':raw_data[i]['ACZ'],'GYX': raw_data[i]['GYX'],'GYY':raw_data[i]['GYY'], "GYZ": raw_data[i]['GYZ']}
+            processed_data_str+=feature_vector+"\n"
+            processed_data.append({'ACX': raw_data[i]['ACX'], 'ACY':raw_data[i]['ACY'], 'ACZ':raw_data[i]['ACZ'],'GYX': raw_data[i]['GYX'],'GYY':raw_data[i]['GYY'], "GYZ": raw_data[i]['GYZ']}'\n')
         # Save calibration values and return
         if(save_gyro_cal):
-            save_calibration()
+            self.save_gyro_calibration()
+
+        if(save_to_file):
+            path = filename.strip().split("/")
+            path[-1] = "data_processed.txt"
+            new_path = ""
+            for element in path:
+                new_path+=element+"/"
+            f = open(new_path[:-1], "w")
+            f.write(processed_data_str)
+            f.close()
         return processed_data, times
+
+
+if __name__ == '__main__':
+    postprocessing = PostProcessing()
+    postprocessing.postprocess_file('../../../data/data_raw.txt', new_gyro_cal = True, save_gyro_cal = True, save_to_file = True)
+    postprocessing.load_gyro_calibration()
